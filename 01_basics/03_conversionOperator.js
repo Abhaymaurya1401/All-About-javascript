c5c6aef7174ec2a8c1d87e56d478e7155afb57c3
Abhay Maurya <156987222+Abhaymaurya1401@users.noreply.github.com>
2024-06-12 11:17:09 +0000

    operations

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -17,6 +17,39 @@ let isLoggedIn = "abhay"
 let booleanIsLoggedIn =  Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
+ //+++++++++++++++++++++++++ Operaions ++++++++++++++++++++
+
+ let value =3
+ let negValue = -value
+ //console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1= "hello"
+let str2 ="  abhay"
+let str3 =str1+str2
+
+//console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+6)
+// console.log(1+2+"2")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1 ,num2,num3
+ 
+num1 = num2 = num3 = 2+2
+
+let gameCounter =100;
+gameCounter++;
+console.log(gameCounter);
 
 //  1 => true; 0 => false
 // " => false"
