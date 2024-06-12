59c650ac7e09a0117390837da63e50fe0f47f710
Abhay Maurya <156987222+Abhaymaurya1401@users.noreply.github.com>
2024-06-12 02:52:42 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+let accountId =1234
+const acountEmail ="abhay@123"
+var accountCity ="delhi"
+
+console.log(accountId);
+
+/*
+prefer not to user var 
+because of issue in block space and functional scope
+*/
+
+console.table([accountId,accountEail,accountId])
