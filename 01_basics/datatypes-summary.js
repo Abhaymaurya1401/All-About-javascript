21334dab45274e9f40d655c0e81eae82c2385b41
Abhay Maurya <156987222+Abhaymaurya1401@users.noreply.github.com>
2024-06-13 01:32:14 +0000

    change in location

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// Primitive
+
+//  7 types:
+//String, Number,Boolean, null, undefined,symbol,BigInt
+
+const score =100;
+const scoreValue =100.3;
+
+const isLoggedIn= false;
+const outsideTemp =null
+let userEmail;
+
+
+const id =Symbol('123')
+const anotherId =Symbol('123')
+
+// console.log(id ===anotherId);
+
+ const bigNumber = 2232235645657n
+
+
+
+//Reference Data type (Non primitive)
+//Array ,Object,Functios
+
+
+const heros =["saktiman","ninha","krish"];
+let muObj
+ ={
+    name: "hitesh",
+    age:22,
+ }
+
+ const myFunction = function(){
+    console.log("hello world")
+ }
+
+ //console.log(typeof bigNumber)
+ console.log(typeof muObj )
+
+
+ // https://262.ecma-international.org//5.1/#sec-11.4.3
